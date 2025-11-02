dbf726757e01f2f2118a386d52937df31c7df7a6
Peyton Smith <psmith@iir.com>
2025-11-02 18:25:37 -0500

    rename env variable

--- zillowguessr/src/app/play/page.tsx
+++ zillowguessr/src/app/play/page.tsx
@@ -24,7 +24,7 @@ import { useTheme } from "next-themes";
 import "@/styles/app.css";
 import "@/styles/leaderboard.css";
 
-const ROUNDS = Number(process.env.NUMBER_OF_ROUNDS) || 5;
+const ROUNDS = Number(process.env.NEXT_PUBLIC_NUMBER_OF_ROUNDS) || 5;
 
 type PropertyInfo = {
   urls: string[];
